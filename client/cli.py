dcc1367d9089d0a5c8d1597f0114638d4dfa0bed
luoruofeng <ruofeng.luo@dmall.com>
2022-01-06 15:07:14 +0800

    struct upload

--- client/cli.py
+++ client/cli.py
@@ -1,84 +1,91 @@
 import socket
 import threading
-import time
+import re
 from array import array
 from pathlib import Path
 from os.path import exists, isdir, getsize
 from os import mkdir
-from threading import Lock
 from json import loads,dumps
+from struct import *
 
-BS = 32768000
-lock = Lock()
-name = "NAME"
-size = "SIZE"
-done = "DONE"
-last = "1"
-not_last = "0"
+BS = 1 << 10
+HEAD_LEN = 1 << 8
+TARGET_DIR = "c://Users//luoruofeng//Desktop"
 
 class Client:
     def __init__(self):
         self.s = socket.create_connection(("127.0.0.1",8888,))
-        self.p = Path("c://Users//luoruofeng//Desktop", str(self.s.fileno()))
-        if not exists(self.p):
-            mkdir(self.p)
+        self.tp = Path(TARGET_DIR, str(self.s.fileno()))
+        if not exists(self.tp):
+            mkdir(self.tp)
         threading.Thread(target=self.recv, args=()).start()
 
-    def get_file_info(self) -> tuple:
-        info_byte = self.s.recv(1024)
-        info_str = info_byte.decode("utf-8")
-        print(info_str)
-        if "name" in info_str and "size" in info_str:
-            info = loads(info_str)
-            n = info["name"]
-            s = info["size"]
-            return (n, s,)
-        else:
-            raise RuntimeError("info wrong!")
+    def check_first_chunk(self, content: bytes) -> tuple:
+        r = re.compile(b"^{.*name.*size.*}").match(content)
+        if r == None:
+            return None
+        head = r.group(0)
+        if not head:
+            return None
+        data = content[HEAD_LEN:]
+        info = loads(head.decode())
+        name = info["name"]
+        size = info["size"]
+        return (name, size, data)
 
 
     def recv(self):
+        name, size = None, None
         while True:
-            n, s = self.get_file_info()
-            while True:
-                try:
-                    pc = self.s.recv(BS)
-                    if len(pc) <= 0:
-                        print("remote server close connection!")
-                        return
-                    target_file = Path(self.p, n)
-                    with open(target_file, "ab+") as f:
-                        f.write(pc)
-                        f.flush()
-                        if pc is not None:
-                            print(str(len(pc)))
-                        if getsize(target_file) >= s:
-                            self.s.send(last.encode())
-                            print(f"FILE SIZE IS {getsize(target_file)}. FILE NAME:{n}. GET ALL FILE.")
-                            break
-                        else:
-                            self.s.send(not_last.encode())
-                except (ConnectionError,ConnectionResetError) as e:
-                    print("remote server close connection!")
+            try:
+                data = self.s.recv(BS, socket.MSG_WAITALL)
+                if len(data) <= 0:
+                    print("CONNECTION OF REMOTE SERVER FAILED!")
                     return
-            print("done!")
+                head = self.check_first_chunk(data)
+                if head is not None:
+                    name, size, data = head[0], head[1], head[2]
+                target_file = Path(self.tp, name)
+                with open(target_file, "ab+") as f:
+                    f.write(data)
+                    f.flush()
+                    if data is not None:
+                        print(str(len(data)))
+                    print("-")
+                    if getsize(target_file) >= size:
+                        print(f"FILE SIZE IS {getsize(target_file)}. FILE NAME:{name}. GET ALL FILE.")
+            except (ConnectionError,ConnectionResetError) as e:
+                print("CONNECTION OF REMOTE SERVER FAILED!")
+                return
+        print("DONE!")
+
+
+    def get_send_size(self, data):
+        content_size = HEAD_LEN + len(data)
+        remainder = content_size % BS
+        if remainder != 0:
+            content_size = content_size + (BS - remainder)
+        return content_size
+
+    def get_head(self, path, send_size) -> bytes:
+        head_dict = {"name": path.name, "size": send_size}
+        return pack(f"{HEAD_LEN}s".encode(), dumps(head_dict).encode("utf-8"))
+
+    def content(self, head, data, send_size):
+        return pack(f"{send_size}s".encode(), head + data)
 
     def start(self):
         while True:
             content = input("input:\n")
-            lock.acquire()
             p = Path(content)
             if exists(p) and not isdir(p):
-                info = {"name": p.name,"size": getsize(p)}
-                self.s.send(dumps(info).encode("utf-8"))
-                first_resp = self.s.recv(1024).decode("utf-8")
-                if first_resp != done:
-                    raise RuntimeError("file info send failed!")
                 with open(p, "rb+") as f:
-                    c = f.read()
-                    print(f"send data:{len(c)}")
-                    self.s.send(c)
-            lock.release()
+                    data = f.read()
+                    send_size = self.get_send_size(data)
+                    head = self.get_head(p, send_size)
+                    content = self.content(head, data, send_size)
+                    print(f"SEND SIZE:{len(content)},HEAD SIZE:{len(head)} FIZE SIZE{len(data)}:")
+                    self.s.sendall(content)
 
 if __name__=="__main__":
     Client().start()
--- core/server.py
+++ core/server.py
@@ -7,7 +7,7 @@ from loguru import logger
 from json import loads
 
 done = "DONE"
-BS = 32768000
+BS = 1024
 
 class Server:
     def __init__(self):
@@ -28,7 +28,6 @@ class Server:
             i.close()
         self.s.close()
 
-
     def recv(self):
         while True:
             c, a = self.s.accept()
@@ -36,61 +35,31 @@ class Server:
             threading.Thread(target=self.mes_handle,args=(c, a,)).start()
         self.end()
 
-    def send_part(self, pc, handle_result):
+    def send_part(self, pc):
         for addr, cs in self.addr_cs.items():
-            print(str(addr))
             try:
                 cs.sendall(pc)
-                has_done = bool(int(cs.recv(1024).decode("utf-8")))
-                if bool(has_done):
-                    handle_result.append((addr, True))
             except Exception as e:
-                logger.info(f"recver's connection closed! addr:{addr}")
-                handle_result.append((addr, False))
-                if addr in self.addr_cs:
-                    del self.addr_cs[addr]
+                logger.info(f"CONNECTION OF RECEVER IS CLOSED! ADDR:{addr}")
+                self.lose_conn(cs, addr)
                 continue
 
-
-    def get_file_info(self, c: socket.socket, a: str) -> tuple:
-        info_byte = c.recv(1024)
-        info_str = info_byte.decode("utf-8")
-        if "name" in info_str and "size" in info_str:
-            for addr, cs in self.addr_cs.items():
-                cs.send(info_byte)
-            c.send(done.encode("utf-8"))
-            info = loads(info_str)
-            name = info["name"]
-            size = info["size"]
-            return (name, size,)
-        else:
-            raise RuntimeError("INFO IS WRONG!")
+    def lose_conn(self, cs, addr):
+        logger.info(f"LOSE CONNECTION! ADDR:{addr}")
+        if addr in self.addr_cs:
+            del self.addr_cs[addr]
+        cs.close()
 
     def mes_handle(self,c: socket.socket, a: tuple):
-        logger.info(str(a)+" connected!")
+        logger.info(str(a)+" HAVE CONNECTED!")
         while True:
-            name, size = self.get_file_info(c, a)
-            logger.info(f"GET INFO FROM {str(a)}!")
-            handle_result = []
-            while True:
-                try:
-                    pc = c.recv(BS)
-                except Exception as err:
-                    #TODO 发送人 断开 需要告诉其他人 不用接收了 可以删除刚才的或者什么都不做
-                    logger.info(str(err))
-                    del self.addr_cs[a]
-                    c.close()
-                    return
-                try:
-                    self.send_part(pc, handle_result)
-                    #TODO 这样写代码还是有纰漏 因为send_part没有全部发送就会执行join
-                except:
-                    continue
-                if len(self.addr_cs) == len(handle_result):
-                    break
-                    logger.info(f"SEND DONE! result:{handle_result}")
-        self.allc.remove(c)
-        c.close()
+            try:
+                pc = c.recv(BS, socket.MSG_WAITALL)
+            except (ConnectionError, ConnectionResetError) as e:
+                self.lose_conn(c, a)
+                return
+            self.send_part(pc)
+        self.lose_conn(c, a)
 
 
 if __name__=="__main__":
